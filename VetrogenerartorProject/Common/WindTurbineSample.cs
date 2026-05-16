e30b5396c8fa053fbfc1efa2e870255f97c771df
Boško Milićev <boskomilicev04@gmail.com>
2026-05-16 15:33:54 +0200

    Add: WindTurbineSample model and IWindTurbineService contract

---
+++ VetrogenerartorProject/Common/WindTurbineSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Common
+{
+    [DataContract]
+    public class WindTurbineSample
+    {
+        [DataMember] public DateTime Timestamp { get; set; }
+        [DataMember] public double WindSpeed { get; set; }
+        [DataMember] public double WindDirection { get; set; }
+        [DataMember] public double NacellePosition { get; set; }
+        [DataMember] public double PowerKW { get; set; }
+        [DataMember] public double PotentialPowerDefaultKW { get; set; }
+        [DataMember] public double PowerFactor { get; set; }
+        [DataMember] public double ReactivePowerKvar { get; set; }
+        [DataMember] public double GridFrequencyHz { get; set; }
+        [DataMember] public double GeneratorRpm { get; set; }
+        [DataMember] public int RowIndex { get; set; }
+        [DataMember] public string TurbineId { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IWindTurbineService
+    {
+        [OperationContract]
+        void StartSession(string turbineId);
+
+        [OperationContract]
+        void PushSample(WindTurbineSample sample);
+
+        [OperationContract]
+        void EndSession();
+    }
+}
